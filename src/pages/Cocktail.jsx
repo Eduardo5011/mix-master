5117db47b0a5e41d42f5dfd638a330b545a71fa7
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-09-08 17:25:35 -0400

    updated cocktail component

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useRouteLoaderData, Link, useLoaderData } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailPage";
+import axios from "axios";
+
+const singleCocktailUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+
+  return { id, data };
+};
 
 const Cocktail = () => {
-  return (
-    <h1>Cocktail</h1>
-  )
-}
+  const { id, data } = useLoaderData();
+  return <h1>Cocktail</h1>;
+};
 
-export default Cocktail
+export default Cocktail;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useNavigation } from "react-router-dom";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {isPageLoading ? <div className="loading" /> : <Outlet />}
       </section>
     </>
   );
