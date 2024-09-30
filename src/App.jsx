bd31b9d353d8dcbd0e133e407e1abc6addfa319c
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-09-30 15:50:07 -0400

    updated newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import {
 
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleCocktailLoader } from "./pages/Cocktail";
+import { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
   {
@@ -27,13 +28,14 @@ const router = createBrowserRouter([
       },
       {
         path: "cocktail/:id",
-        errorElement: <SinglePageError/>,
+        errorElement: <SinglePageError />,
         loader: singleCocktailLoader,
         element: <Cocktail />,
       },
       {
         path: "newsletter",
         element: <Newsletter />,
+        action: newsletterAction,
       },
       {
         path: "about",
--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Form, redirect } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
+
+export const action = async ({request})=>{
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  const response = await axios.post(newsletterUrl, data)
+  
+  console.log(response)
+  toast.success(response.data.msg)
+  return redirect('/')
+}
 
 const Newsletter = () => {
   return (
-    <form className="form">
+    <Form className="form" method="POST">
       <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
         Our newsletter
       </h4>
@@ -15,10 +30,41 @@ const Newsletter = () => {
           className="form-input"
           name="name"
           id="name"
-          defaultValue=""
+          defaultValue="john"
         />
       </div>
-    </form>
+      <div>
+        <label htmlFor="lastName" className="form-label">
+          Last Name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="smith"
+        />
+      </div>
+      <div>
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </Form>
   );
 };
 
