34d09409e9f7c9eefe869a085b26ba5c4e8e4166
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-09-30 14:10:39 -0400

    updated newsletter

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Newsletter = () => {
   return (
-    <h1>Newsletter</h1>
-  )
-}
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        Our newsletter
+      </h4>
+      <div>
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue=""
+        />
+      </div>
+    </form>
+  );
+};
 
-export default Newsletter
+export default Newsletter;
