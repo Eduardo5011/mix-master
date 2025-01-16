de3270c25d121fd54aa4cb7d279892a7dba1e0b2
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-01-15 22:32:57 -0500

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 
 import {
   About,
@@ -59,7 +59,6 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />;
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
